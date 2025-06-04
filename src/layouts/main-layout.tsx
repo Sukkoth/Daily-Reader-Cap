78fbd3d5d543364673f5f35c792153060b0c992a
Sukkoth <suukootj@gmail.com>
2025-06-04 10:48:40 +0300

    refactor: color re-arrange to make every page consistent

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -3,7 +3,7 @@ import BottomNav from "../bottom-nav";
 
 function MainLayout() {
   return (
-    <main className="bg-gradient-to-b max-w-2xl mx-auto dark:from-gray-900/80 dark:to-gray-950/65 from-gray-50 to-gray-50 text-black dark:text-white flex flex-col min-h-[100dvh]">
+    <main className="mx-auto flex max-w-2xl flex-col">
       <section className="flex-grow">
         <Outlet />
       </section>
