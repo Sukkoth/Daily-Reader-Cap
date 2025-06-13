804bdb0968ab207fd47d61248e6bd8272923c957
Sukkoth <suukootj@gmail.com>
2025-06-13 11:45:29 +0300

    fix: adjust layout and styling in WeeklyStats component for improved consistency

--- src/home-stats/weekly-stats.tsx
+++ src/home-stats/weekly-stats.tsx
@@ -4,15 +4,15 @@ function WeeklyStats() {
   const days = ["S", "M", "T", "W", "T", "F", "S"];
   const activeDays = [0, 1, 2, 3, 4];
   return (
-    <div className="flex gap-4 justify-between items-end">
+    <div className="flex items-end justify-between gap-4">
       {days.map((day, index) => (
         <div className="flex flex-col items-center gap-1" key={index}>
           <div
             className={cn(
-              "w-7  h-[6px] rounded-xl",
+              "h-[6px] w-7 rounded-xl",
               activeDays.includes(index)
                 ? "bg-gradient-to-r from-orange-400 to-orange-500"
-                : "bg-gray-500"
+                : "bg-gray-300 dark:bg-gray-500",
             )}
           ></div>
           <p className="text-xs">{day}</p>
