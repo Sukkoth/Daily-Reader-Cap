67ef27c4beafeee7b5cf0d04816bbd548b527755
Sukkoth <suukootj@gmail.com>
2025-06-13 11:46:09 +0300

    fix: enhance layout and styling in MainLayout component for improved usability

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -3,8 +3,8 @@ import BottomNav from "../bottom-nav";
 
 function MainLayout() {
   return (
-    <main className="mx-auto flex h-[100dvh] max-w-2xl flex-col">
-      <section className="flex-grow">
+    <main className="mx-auto flex h-[100dvh] max-w-2xl flex-col pt-5">
+      <section className="flex-grow overflow-y-auto">
         <Outlet />
       </section>
       <BottomNav />
