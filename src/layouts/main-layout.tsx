df57f5d0e1dd082861f5aafccd687e415532c343
sukkoth <suukootj@gmail.com>
2025-06-13 15:34:12 +0300

    fix: remove top padding from the web and make it display only on native devices

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
 import BottomNav from "../bottom-nav";
+import { Capacitor } from "@capacitor/core";
+import { cn } from "../utils/cn";
 
 function MainLayout() {
   return (
-    <main className="mx-auto flex h-[100dvh] max-w-2xl flex-col pt-5">
+    <main
+      className={cn("mx-auto flex h-[100dvh] max-w-2xl flex-col", {
+        "pt-5": Capacitor.isNativePlatform(),
+      })}
+    >
       <section className="flex-grow overflow-y-auto">
         <Outlet />
       </section>
