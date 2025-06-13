c84cb5a1608f8f065e3f77ff1c2e173f14c611c4
Sukkoth <suukootj@gmail.com>
2025-06-13 11:53:14 +0300

    fix: enhance layout and styling in BottomSheetItem component for improved visual consistency

--- src/bottom-sheet-setting/index.tsx
+++ src/bottom-sheet-setting/index.tsx
@@ -12,9 +12,11 @@ export default function BottomSheetItem({ onClose }: Props) {
         className="pointer-events-auto fixed inset-0 z-40"
       />
 
-      <Sheet.Container className="z-50">
-        <Sheet.Header />
-        <Sheet.Content>{/* Sheet content */}</Sheet.Content>
+      <Sheet.Container>
+        <Sheet.Header className="bg-light-2 dark:bg-shade-2 z-50" />
+        <Sheet.Content className="bg-light-2 dark:bg-shade-2">
+          <h1>Hello World</h1>
+        </Sheet.Content>
       </Sheet.Container>
     </Sheet>
   );
